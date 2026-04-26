625035d43882be7b5400837331204c3f9f7e15f7
Krystian Wasylik <418573@zarz.agh.edu.pl>
2026-04-26 16:43:49 +0200

    Revert "masakra kurwa"
    This reverts commit 3fa23eb5991d300d161d35bf33b06580dc10573f.

--- Order.cs
+++
@@ -1,7 +0,0 @@
-﻿public class Order
-{
-    public int Id { get; set; }
-    public DateTime OrderDate { get; set; }
-    public decimal TotalAmount { get; set; }
-    public int CustomerId { get; set; }
-}
