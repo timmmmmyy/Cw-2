d05e80aa769457535bfb62dd784732076dc3341c
Krystian Wasylik <418573@zarz.agh.edu.pl>
2026-04-26 16:47:21 +0200

    tym razem sie uda

---
+++ Order.cs
@@ -0,0 +1,7 @@
+﻿public class Order
+{
+    public int Id { get; set; }
+    public DateTime OrderDate { get; set; }
+    public decimal TotalAmount { get; set; }
+    public int CustomerId { get; set; }
+}
